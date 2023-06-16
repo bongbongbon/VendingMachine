c2489561d57f9b3f80ac8a9f2012fe6fd7ff6524
Kwakyoung <128439473+Kwakyoung@users.noreply.github.com>
2023-06-16 11:12:10 +0900

    11

--- app/src/main/java/com/example/myapplication/InfoActivity.java
+++ app/src/main/java/com/example/myapplication/InfoActivity.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity {
 
+        Button btn_back1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        btn_back1 = findViewById(R.id.btn_back1);
+
+        btn_back1.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+        });
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/LoginDTO.java
+++ app/src/main/java/com/example/myapplication/LoginDTO.java
@@ -1,11 +1,14 @@
 package com.example.myapplication;
 
 public class LoginDTO {
-    private String id, pw;
+    private String id, pw, name, age, money;
 
-    public LoginDTO(String id, String pw) {
+    public LoginDTO(String id, String pw, String name, String age, String money ) {
         this.id = id;
         this.pw = pw;
+        this.name = name;
+        this.age = age;
+        this.money = money;
     }
 
     public String getId() {
@@ -22,5 +25,29 @@ public class LoginDTO {
 
     public void setPw(String pw) {
         this.pw = pw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
     }
 }
