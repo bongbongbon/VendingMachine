0fd857b6aa3d079dcb5a94ab40e942508adddd57
Kwakyoung <128439473+Kwakyoung@users.noreply.github.com>
2023-06-16 10:08:32 +0900

    0616

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,16 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_store, btn_info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
+        btn_store = findViewById(R.id.btn_store);
+        btn_info = findViewById(R.id.btn_info);
+
+        btn_store.setOnClickListener(v -> {
+            Intent intet = new Intent(this,StoreActivity.class  );
+            startActivity(intet);
+        });
+
+        btn_info.setOnClickListener(v -> {
+            Intent intent = new Intent(this,InfoActivity.class );
+            startActivity(intent);
+        });
+
+
+
     }
 
 }
